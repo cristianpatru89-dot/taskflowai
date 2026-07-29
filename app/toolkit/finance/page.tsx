57cdf7b476a7b5230da8a75e500299ec9abbcab1
Cristian Patru <cristianpatru@NB-FP-0007.local>
2026-07-29 11:08:06 +0300

    add consistent Navbar component across all pages

--- app/toolkit/finance/page.tsx
+++ app/toolkit/finance/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Navbar from '../../../components/navbar';
+import Navbar from '@/components/navbar';
 
 import { useState } from 'react'
 
--- app/toolkit/pm/page.tsx
+++ app/toolkit/pm/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Navbar from '../../../components/navbar';
+import Navbar from '@/components/navbar';
 
 // Note: metadata must be in a separate file for client components
 
